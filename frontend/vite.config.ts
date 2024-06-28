43db8ffca5ff1314d031736470a5aa506db14151
kwiats <pawuskwiatkowski@gmail.com>
2024-06-28 23:01:08 +0200

    [pk] - added dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/",
+    plugins: [react()],
+    preview: {
+        port: 4200,
+        strictPort: true,
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        port: 4200,
+        strictPort: true,
+        host: true,
+    },
 })
